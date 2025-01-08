5747b53a6532dfa8cd5c3df8e71270d472ba186b
karthikeyan-ks <karthikeyanks3673@gmail.com>
2025-01-08 22:15:12 +0530

    backend it integrated with audio separation module

--- backend/api/tasks.py
+++ backend/api/tasks.py
@@ -1,9 +1,11 @@
 from __future__ import absolute_import, unicode_literals
 from celery import shared_task
+from src.input.file_reader import read_audio #export PYTHONPATH="/home/karthikeyan/code/MainProject/freq-split-enhance:$PYTHONPATH" for exporting the module 
+
 import time
 
 @shared_task
 def process_uploaded_file(file_path):
     # Simulate long-running task
-    time.sleep(300)  # Replace with actual file processing logic
+    read_audio(file_path=file_path)
     return 'File processed'
