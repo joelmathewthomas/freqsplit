2256989fdad04ec343ba43a47d6a91b33fe20e99
Joel Mathew Thomas <90510078+joelmathewthomas@users.noreply.github.com>
2025-02-25 23:34:54 +0530

    check file format in /api/upload/

--- api/api/views.py
+++ api/api/views.py
@@ -4,6 +4,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from .tasks import save_and_classify
+from freqsplit.input.format_checker import is_supported_format
 
 UPLOAD_DIR = "/tmp/freqsplit"
 
@@ -17,6 +18,10 @@ def upload_audio(request):
         return Response({"Error: No file provided"}, status=status.HTTP_400_BAD_REQUEST)
     
     audio_file = request.FILES['file']
+    
+    # Check file format before proceeding
+    if not is_supported_format(audio_file.name):
+        return Response({"error": "Unsupported file format"}, status=status.HTTP_400_BAD_REQUEST)
     
     # Generate a unique ID for this upload
     file_uuid = str(uuid.uuid4())[:8]
